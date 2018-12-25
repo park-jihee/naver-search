50f5f1cf39c4cfcd09bbc14e12d619460600da39
park-jihee <brs0705@naver.com>
2018-12-26 00:12:59 +0900

    naver search

---
+++ naver.php
@@ -0,0 +1,24 @@
+<?php
+$sch = "";
+
+echo $sch;
+if( isset($_GET['sch']) ) $sch = $_GET['sch'];
+$news = "http://newssearch.naver.com/search.naver?where=rss&query={$sch}&field=0&nx_search_query=&nx_and_query=&nx_sub_query=&nx_search_hlquery=&is_dts=0";
+$xml = simplexml_load_file($news);
+//var_dump($xml);
+$channel = $xml->channel;
+$newslist = $channel->item;
+$data = array();
+$no = 0;
+foreach( $newslist as $news ){
+	$data[$no] = array(
+		'title'=> $news->title,
+		'author'=>$news->author,
+		'link'=>$news->link,
+		'category' => $news->category,
+		'pubDate' => $news->pubDate
+	);
+	$no++;
+}
+$json = json_encode($data, JSON_UNESCAPED_UNICODE);
+echo $json;
